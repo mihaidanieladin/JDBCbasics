aa24dea7566a5b8e592fee5452ccf946ae836ed0
Mihai Daniel Adin <mihaidanieladin@gmail.com>
2020-01-25 18:32:16 +0200

    add-person-type

---
+++ src/main/java/Main/Person.java
@@ -0,0 +1,56 @@
+package Main;
+
+// Defining a Person type object
+public class Person {
+    
+    // Defining person attributes
+    private int id_person;
+    private String name;
+    private String surname;
+    private String email;
+    private String telephone;
+    
+    
+    // GETTERS AND SETTERS
+    public int getId_person() {
+        return id_person;
+    }
+
+    public void setId_person(int id_person) {    
+        this.id_person = id_person;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+    
+    
+}
