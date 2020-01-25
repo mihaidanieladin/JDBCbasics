bcdd13da3d5d45195a238b5b8d575f194e300df9
Mihai Daniel Adin <mihaidanieladin@gmail.com>
2020-01-25 18:20:33 +0200

    create-connection-db-file

---
+++ src/main/java/Main/Conection.java
@@ -0,0 +1,50 @@
+package Main;
+
+// import sql library
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+// Connection with sql database
+public class Conection {
+    
+    // Define variables to access the DB
+    // Conecting to a mySQL database called 'test'
+    private static final String SQL_URL = "jdbc:sql://localhost/test?useSSL=false?serverTimezone=UTC";
+    private static final String SQL_USER = "root";
+    private static final String SQL_PASS = "admin";
+    
+    // Connecting to the DB
+    public static Connection getConnection() throws SQLException{
+        
+        // Configurate the sql driver and connect the DB passing the variables
+        return DriverManager.getConnection(SQL_URL, SQL_USER, SQL_PASS);
+    }
+    
+    // Close method for ResultSet 
+    public static void close(ResultSet result){
+        try {
+            result.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+    
+    // Close method for PreparedStatement
+    public static void close(PreparedStatement statement){
+        try {
+            statement.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+    
+    // Close method for the Connection from the DB
+    public static void close(Connection connect){
+        try {
+            connect.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+}
--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -7,5 +7,7 @@ package Main;
  * This are the Java Database conection JDBC basics
  */
 public class Main {
-    
+    public static void main(String[] args) {
+        
+    }
 }
