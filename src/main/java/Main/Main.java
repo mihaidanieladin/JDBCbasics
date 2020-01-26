e1277a2b6fa40029397118c69bb481ad077f6f03
Mihai Daniel Adin <mihaidanieladin@gmail.com>
2020-01-26 15:43:43 +0200

    add-comments-to-code

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -9,21 +9,31 @@ import java.util.List;
  * 
  * This are the Java Database conection JDBC basics
  */
+
+// Main method that runs the program
 public class Main {
     public static void main(String[] args) {
+        
+        // New database object
         Database db = new Database();
+        // Adding persons from the DB in a Person type obj list
         List<Person> persons = db.select();
         
+        // Iterating persons from DB
         for(Person pers : persons){
+            // Print in the console the person
             System.out.println("pers = " + pers);
         }
         
+        
+        //Creating a new Perosn object
         Person person = new Person();
+        // Initializing the person attributes
         person.setName("Pepe");
         person.setSurname("Gonzalo Garcia");
         person.setEmail("ppgnzlo@gmail.com");
         person.setTelephone("642777321");
-        
+        // Insert new person created previous and add to the DB
         db.insert(person);
     }
 }
