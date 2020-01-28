55f61bfff504c906ac949bc4535a54a7f15e7b72
Mihai Daniel Adin <mihaidanieladin@gmail.com>
2020-01-28 22:33:23 +0200

    add-person-commandline

--- src/main/java/Main/Command.java
+++ src/main/java/Main/Command.java
@@ -1,5 +1,9 @@
 package Main;
 
+import DB.Insert;
+import DB.Remove;
+import DB.Select;
+import DB.Update;
 import java.util.Scanner;
 import DB.UserAuth;
 import java.util.ArrayList;
@@ -30,15 +34,22 @@ public class Command {
                     switch(inputUser){
                         case 1:
                             getUsers();
+                            newOp();
                             break;
                         case 2:
                             addUser();
+                            newOp();
                             break;
                         case 3:
                             updateUser();
+                            newOp();
                             break;
                         case 4:
                             deleteUser();
+                            newOp();
+                            break;
+                        default:
+                            startCommand();
                             break;
                     }
                     break;
@@ -48,14 +59,26 @@ public class Command {
                             + "2.Insert person \n"
                             + "3.Update person \n"
                             + "4.Delete person");
-                    switch(option){
+                    int inputPerson = sc.nextInt();
+                    switch(inputPerson){
                         case 1:
+                            getPersons();
+                            newOp();
                             break;
                         case 2:
+                            addPerson();
+                            newOp();
                             break;
                         case 3:
+                            updatePerson();
+                            newOp();
                             break;
                         case 4:
+                            deletePerson();
+                            newOp();
+                            break;
+                        default:
+                            startCommand();
                             break;
                     }
             }
@@ -64,6 +87,23 @@ public class Command {
             System.out.print(err);
         }
     }
+    
+    public static void newOp(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("New operation? y/n");
+        String r = sc.nextLine();
+        switch(r) {
+            case "y":
+                startCommand();
+                break;
+            case "n":
+                System.exit(0);
+            default:
+                newOp();
+        }
+        
+    }
+    
     public static void getUsers(){
         UserAuth userDB = new UserAuth();
         List<User> users = new ArrayList<User>();
@@ -80,11 +120,12 @@ public class Command {
         
         System.out.println("New user name:");
         String username = sc.nextLine();
-        user.setUsername(username.toString());
         
         System.out.println("New password:");
         String password = sc.nextLine();
-        user.setPassword(password.toString());
+        
+        user.setPassword(password);
+        user.setUsername(username);
         
         userDB.insertUser(user);
         System.out.println("New user sucessfuly added to DB!");
@@ -97,17 +138,15 @@ public class Command {
         
         System.out.println("Id user's to update:");
         int id_user = sc.nextInt();
-        System.out.println("usrID = " + id_user);
         System.out.println("New user name:");
-        String username = sc.next();
-        System.out.println("username = " + username);
+        String username = sc.nextLine();
         System.out.println("New password:");
-        String password = sc.next();
-        System.out.println("password = " + password);
+        String password = sc.nextLine();
         
         user.setPassword(password);
         user.setUsername(username);
         user.setId_user(id_user);
+        
         userDB.updateUser(user);
         System.out.println("User sucessfuly updated!");
         
@@ -125,4 +164,86 @@ public class Command {
         userDB.removeUser(user);
         System.out.println("User sucessfuly deleted!");
     }
+
+    private static void getPersons() {
+        List<Person> persons = new ArrayList<Person>();
+        Select selectDB = new Select();
+        persons = selectDB.select();
+                
+        for(Person pers: persons) {
+            System.out.println("Person: " + pers);
+        }
+    }
+
+    private static void addPerson() {
+        Scanner sc = new Scanner(System.in);
+        Insert insertDB = new Insert();
+        Person person = new Person();
+        
+        System.out.println("New person name:");
+        String username = sc.nextLine();
+
+        System.out.println("New password:");
+        String surname = sc.nextLine();
+
+        System.out.println("New email:");
+        String email = sc.nextLine();
+
+        System.out.println("New Telephone:");
+        String telephone = sc.nextLine();
+        
+        
+        person.setName(username);
+        person.setSurname(surname);
+        person.setEmail(email);
+        person.setTelephone(telephone);
+        
+        insertDB.insert(person);
+        System.out.println("New user sucessfuly added to DB!");
+    }
+
+    private static void updatePerson() {
+        Scanner sc = new Scanner(System.in);
+        Update updateDB = new Update();
+        Person person = new Person();
+        
+        System.out.println("Person id to update:");
+        int id_person = sc.nextInt();
+        
+        System.out.println("New person name:");
+        String username = sc.next();
+
+        System.out.println("New password:");
+        String surname = sc.next();
+
+        System.out.println("New email:");
+        String email = sc.next();
+
+        System.out.println("New Telephone:");
+        String telephone = sc.next();
+        
+        
+        person.setName(username);
+        person.setSurname(surname);
+        person.setEmail(email);
+        person.setTelephone(telephone);
+        person.setId_person(id_person);
+        
+        updateDB.update(person);
+        System.out.println("New person sucessfuly updated to DB!");
+    }
+
+    private static void deletePerson() {
+        Scanner sc = new Scanner(System.in);
+        Remove removeDB = new Remove();
+        Person person = new Person();
+        
+        System.out.println("Person id to delete:");
+        int id_person = sc.nextInt();
+        person.setId_person(id_person);
+        
+        removeDB.remove(person);
+        System.out.println("New person sucessfuly deleted from DB!");
+    }
+   
 }
